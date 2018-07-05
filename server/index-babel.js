54be034edc46e54be934a98a26623917be184332
Tran Tin <tin.tran@neuromeka.com>
2018-07-05 16:58:18 +0900

    Init setup for project with babel-node and nodemon

---
+++ server/index-babel.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get('/test', (req, res) => {
+    res.send({
+        status: "success",
+        message: "you're briliant people."
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}...`);
+})
